e6abc1c0eb1759d5273d6219a8e3390bd077937b
dranoer <dranoer@gmail.com>
2021-10-19 12:10:34 +0330

    add: Compose in log screen

--- app/src/main/java/com/dranoer/oweme/log/LogFragment.kt
+++ app/src/main/java/com/dranoer/oweme/log/LogFragment.kt
@@ -4,9 +4,21 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.stringResource
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.dranoer.oweme.R
 import com.dranoer.oweme.databinding.FragmentLogBinding
+import com.google.accompanist.appcompattheme.AppCompatTheme
 
 class LogFragment : Fragment() {
 
@@ -25,12 +37,29 @@ class LogFragment : Fragment() {
         _binding = FragmentLogBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
+        binding.notYetImplemented.setContent {
+            AppCompatTheme {
+                NotYetImplemented()
+            }
+        }
+
         return root
+    }
+
+    @Composable
+    private fun NotYetImplemented() {
+        Text(
+            text = stringResource(R.string.not_yet_implemented),
+            style = MaterialTheme.typography.h5,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = dimensionResource(R.dimen.margin_small))
+                .wrapContentWidth(Alignment.CenterHorizontally)
+        )
     }
 
     override fun onDestroy() {
         super.onDestroy()
         _binding = null
     }
-
 }
